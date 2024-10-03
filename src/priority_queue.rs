c36cd10d00235ce58d5472687f0e06c354b90d81
Wombatlord <jordan.stone0@gmail.com>
2024-10-03 22:21:49 +0100

    removed redundant code in lowest_cost_edge polling

--- src/priority_queue.rs
+++ src/priority_queue.rs
@@ -24,30 +24,23 @@ impl PriorityQueue {
     }
 
     pub fn lowest_cost_edge(&mut self) -> Edge {
-        let mut lowest_cost_edge: Edge = Edge {
-            origin: Node { x: 0, y: 0 },
-            destination: Node { x: 0, y: 0 },
-            cost: 0,
-        };
         let mut lowest_edge_idx: usize = 0;
         
         for (idx, edge) in self.queue.iter().enumerate() {
             if idx == 0 {
-                lowest_cost_edge = *edge;
                 lowest_edge_idx = idx;
                 continue;
             }
 
-            let min_cost_edge = min(lowest_cost_edge.cost, edge.cost);
-            if lowest_cost_edge.cost == min_cost_edge {
+            let min_cost_edge = min(self.queue[lowest_edge_idx].cost, edge.cost);
+            if self.queue[lowest_edge_idx].cost == min_cost_edge {
                 continue;
             }
 
-            lowest_cost_edge = *edge;
             lowest_edge_idx = idx;
         }
 
-        self.queue.remove(lowest_edge_idx);
-        return lowest_cost_edge;
+        self.queue.remove(lowest_edge_idx)
+        // return lowest_cost_edge;
     }
 }
